0c0efe287b365ec0cc261e7e8b12f2b62eb90ca3
pageza <zap.freelance@gmail.com>
2023-09-19 15:21:44 -0600

    behind

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -4,7 +4,6 @@ import (
 	"net/http"
 
 	"github.com/pageza/chat-app/internal/errors"
-	"github.com/pageza/chat-app/internal/redis"
 	"github.com/sirupsen/logrus"
 )
 
@@ -17,28 +16,6 @@ func RecoveryMiddleware(next http.Handler) http.Handler {
 				errors.RespondWithError(w, errors.NewAPIError(http.StatusInternalServerError, "Internal Server Error"))
 			}
 		}()
-		next.ServeHTTP(w, r)
-	})
-}
-
-// RateLimitMiddleware is a middleware for rate limiting based on IP address
-func RateLimitMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ip := r.RemoteAddr
-		rdb := redis.GetRedisClient() // Assuming you have a GetRedisClient function in your redis package
-
-		allowed, err := redis.CheckRateLimit(ip, rdb)
-		if err != nil {
-			logrus.Errorf("Rate limit check failed: %v", err)
-			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-			return
-		}
-
-		if !allowed {
-			http.Error(w, "Too Many Requests", http.StatusTooManyRequests)
-			return
-		}
-
 		next.ServeHTTP(w, r)
 	})
 }
